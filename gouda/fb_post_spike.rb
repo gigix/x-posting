11b5e99056a70e15a53d87a0a983771c0649e29d
Jeff Xiong <gigix1980@gmail.com>
2011-08-06 11:51:43 +0800

    done spike to post message to friend

--- gouda/fb_post_spike.rb
+++ gouda/fb_post_spike.rb
@@ -1,10 +1,14 @@
 #!/usr/bin/env ruby
 
 require 'net/http'
+require "net/https"
 require 'uri'
 
-res = Net::HTTP.post_form(URI.parse('https://graph.facebook.com/100002722005308/feed'),  
-  {'access_token' => '145634995501895|5bb93fdfb07046ed1c8bab48.1-759534364|DiQUmeetFOTdoJAe_44P7Fzj5OE', 
-    'message' => 'post from ruby'})
+http = Net::HTTP.new('graph.facebook.com', 443)
+http.use_ssl = true
 
-puts res.body
+resp, data = http.post2('100002722005308/feed',  
+  'access_token=145634995501895|5bb93fdfb07046ed1c8bab48.1-759534364|DiQUmeetFOTdoJAe_44P7Fzj5OE' + 
+  "&message=post%20from%20ruby @ #{Time.now.to_f}")
+
+puts resp.body
