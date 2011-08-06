4eb339997ff1a1bd3d192a1f565e6f14bed70cb7
Jeff Xiong <gigix1980@gmail.com>
2011-08-06 12:11:17 +0800

    create a rails application

---
+++ gouda/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gouda::Application.config.secret_token = '80f4e9b5f35de834e4b5e150515908649a4c1c48e4b4c1cd4a92aa09147681d1ea28600eb229d39bde17bc6c26c4443b15b8e7dca52c8a77e32722218769dc15'
